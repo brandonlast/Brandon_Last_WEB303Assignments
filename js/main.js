b221e23dd90c260d510f0adefb410f5b592ab176
brandonlast <133057528+brandonlast@users.noreply.github.com>
2023-09-29 22:21:51 -0400

    fixed issues
    Fixed the content not displaying. and the   logos not loading due to incorrect links.

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // WEB303 Assignment 2
 
 document.addEventListener("DOMContentLoaded", function() {
-    var contentDiv = document.getElementById("content");
+    var divContent = document.getElementById("content");
     var links = document.querySelectorAll("#content-wrapper a");
   
-    function loadContent(url) {
+    function loadDiv(url) {
       var xhr = new XMLHttpRequest();
   
       xhr.open("GET", url, true);
@@ -12,10 +12,17 @@ document.addEventListener("DOMContentLoaded", function() {
   
       xhr.onload = function() {
         if (xhr.status === 200) {
-          contentDiv.style.opacity = 0;
-          contentDiv.innerHTML = xhr.responseText;
-          contentDiv.offsetWidth;
-          contentDiv.style.opacity = 1;
+          
+        var newDiv = document.createElement("div");
+        newDiv.innerHTML = xhr.responseText;
+
+        divContent.style.transition = "opacity 0.5s";
+        divContent.style.opacity = 0;
+
+        setTimeout(function() {
+          divContent.innerHTML = newDiv.innerHTML;
+          divContent.style.opacity = 1;
+        }, 500);
         }
       };
   
@@ -24,17 +31,20 @@ document.addEventListener("DOMContentLoaded", function() {
       };
     }
   
-    function clearContent() {
-      contentDiv.style.opacity = 0;
-      contentDiv.innerHTML = "";
+    function clearDiv() {
+      divContent.style.opacity = 0;
+      divContent.innerHTML = "";
     }
   
     links.forEach(function(link) {
       link.addEventListener("click", function(event) {
         event.preventDefault();
-        var targetUrl = link.getAttribute("href");
-        clearContent();
-        loadContent(targetUrl);
+        var Url = link.getAttribute("href");
+
+        console.log("Link clicked:", Url);
+
+        clearDiv();
+        loadDiv(Url);
       });
     });
   });
