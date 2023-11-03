21294af3c033214760bf13dbffbd8001e6d66496
brandonlast <w0768287@myscc.ca>
2023-11-03 17:22:39 -0400

    updated

--- script.js
+++ script.js
@@ -1,19 +1,65 @@
 /*
-	WEB 303 Assignment 1 - jQuery
-	{Brandon Last}
+    Assignment 05
 */
 
-$(document).ready(function() {
-	//Event Handler
-	$('#salary, #percent').keyup(function() {
-		//the difference between change and keyup, is for keyup, the second a key is let go it updates, but for change you have to click off and onto something else.
-		//calculate amouint you can spend on tech
-		var salary = parseFloat($('#yearly-salary').val());
-		var percent = parseFloat($('#percent').val());
-		var amount = (salary * percent) / 100;
-		
-		//make 2 decimal points
-		$('#amount').text('$' + amount.toFixed(2));
-	});
-	
+$(document).ready(function () {
+    
+    class ContentItem {
+
+
+        constructor(id, name, description, category) {
+            this.id = id;
+            this.name = name;
+            this.description = description;
+            this.category = category;
+        }
+
+        updateContentItem(id, name, description, category) {
+            if (id === this.id) {
+                if (name !== null) {
+                    this.name = name;
+                }
+                if (description !== null) {
+                    this.description = description;
+                }
+                if (category !== null) {
+                    this.category = category;
+                }
+            }
+        }
+        
+
+        toString() {
+            return `
+            <div class="content-item-wrapper" id="content-item-${this.id}">
+                <h4>${this.name}</h4>
+                <p>${this.description}</p>
+                <div>${this.category}</div>
+            </div>
+            `;
+        }
+    }
+
+
+    const ContentItems = [
+        new ContentItem(0, "John Cena", "You cant see me", "Wrestler"),
+        new ContentItem(0, "Jackie Chan", "I do small things. I try to do good things every day", "Actor"),
+        new ContentItem(0, "The Rock", "Can you Smell what the rock is cooking?", "Wrestler"),
+        new ContentItem(0, "Jim Carrey", "B-E-A-utiful." , "Actor"),
+        new ContentItem(0, "Taylor Swift", "No matter what happens in life, be good to people" , "Musician"),
+    ];
+
+    const $contentItemList = $("#content-item-list");
+    ContentItems.forEach(item=> {
+        $contentItemList.append(item.toString());
+    });
+    $(".content-item-wrapper").css({
+        border: "2px solid",
+        width: "400px",
+        padding: "5px",
+        margin: "10px",
+    });
+
 });
+
+
